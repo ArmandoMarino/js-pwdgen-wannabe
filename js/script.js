c3ecc9090ca447e6a3f33cfc707110fc81a3d064
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-02 16:20:49 +0100

    101

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log('JS OK')
+
+
+/* TRACCIA
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+
+
