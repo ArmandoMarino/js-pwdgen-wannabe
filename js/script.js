9a9a8a36916e41ecc9511ff28ebca895dea4f534
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-02 16:45:15 +0100

    all prompt added with final innertext

--- js/script.js
+++ js/script.js
@@ -15,5 +15,16 @@ const targetElement = document.getElementById('target');
 //// console.log(targetElement);
 
 // Chiedi all’utente il suo nome,
-const userName = prompt("Qual'è il tuo nome?");
-console.log(userName);
+const firstName = prompt("Qual'è il tuo nome?", "Armando");
+console.log(firstName);
+
+// Chiedi all’utente il suo cognome,
+const lastName = prompt("Qual'è il tuo cognome?", "Marino");
+console.log(lastName);
+
+// Chiedi all’utente il suo colore preferito,
+const userColor = prompt("Qual'è il tuo colore preferito?", "Verde");
+console.log(userColor);
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+targetElement.innerText = firstName + lastName + userColor + "21";
